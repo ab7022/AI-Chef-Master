aed07ab9a0f3966f1bb2e9899cfa8dd4d91c9fe4
Shubham-Lal <im.shubhamlal@gmail.com>
2024-04-12 20:40:55 +0530

    Portfolio: add Career API

--- 2.Portfolio/backend/api/index.py
+++ 2.Portfolio/backend/api/index.py
@@ -228,6 +228,56 @@ def logoutAPP():
 
     return jsonify({'message': 'Logout successful'}), 200
 
+app.config['UPLOAD_FOLDER'] = 'files'
+@app.route('/career' ,methods = ['POST'])
+def carrer():
+    if request.method =="POST":
+        applied_for = request.form.get("appliedFor")
+        personal = request.form.get("personal")
+        experiences = request.form.get("experiences")
+        education = request.form.get("education")
+        skills = request.form.get('skills')
+        socials_json = request.form.get('socials')
+        socials = json.loads(socials_json) if socials_json else {}
+        all_questions = request.form.get("allQuestions")
+        voluntary_questions = request.form.get("voluntaryDisclosures")
+
+        ALLOWED_EXTENSIONS = {'pdf', 'png', 'jpg', 'jpeg', 'doc', 'docx'}
+        certificates = request.files.getlist("certificates[]")
+        certificate_paths = []
+        saved_files = []
+
+        for certificate in certificates:
+            filename = certificate.filename
+            timestamp = datetime.now().strftime("%Y%m%d-%H%M%S")
+            new_filename = f"{timestamp}-{filename}"
+
+            if '.' in new_filename and new_filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS:
+                file_path = os.path.join(app.config['UPLOAD_FOLDER'], new_filename)
+                certificate.save(file_path)
+                certificate_paths.append(file_path)
+                saved_files.append(file_path)
+            else:
+                for file in saved_files:
+                    try:
+                        os.remove(file)
+                    except OSError as e:
+                        print(f"Error: {file} : {e.strerror}")
+                return jsonify({'message': 'Invalid file format'}), 400
+
+        db.carrers.insert_one({
+            'applied_for': applied_for,
+            'personal': personal,
+            'experience': experiences,
+            'education': education,
+            'skills': skills,
+            'socials': socials,
+            'all_questions': all_questions,
+            'voluntary_questions': voluntary_questions,
+            'certificates': certificate_paths
+        })
+
+        return jsonify({'message':'Application submitted successfully'}), 201
 
 @app.route('/api/dishCreateProcess',methods = ['POST','GET'])
 def  dishCreateProcess():
